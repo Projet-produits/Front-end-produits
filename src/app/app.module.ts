3fd80ba1f5fb13d829f9ed949d95a539133c9874
Mickaël Seffar <mickael.seffar@gmail.com>
2024-03-28 10:43:55 +0100

    Create components -Create produit et addproduit components + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './root/app.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { AddProduitComponent } from './add-produit/add-produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitsComponent,
+    AddProduitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/produits/produits.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produits',
+  templateUrl: './produits.component.html',
+  styleUrls: ['./produits.component.css'] // Change styleUrl to styleUrls
+})
+export class ProduitsComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  constructor() {
+  }
+}
